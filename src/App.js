305f641619eee57ed7a1a7f31fe09dc2819732ca
shaown <34128508+camonanesi123@users.noreply.github.com>
2021-06-21 11:29:10 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { simpleStorage } from "./abi/abi";
+import Web3 from "web3";
+import Nav from "./components/Nav.js";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+ root: {
+   "& > *": {
+     margin: theme.spacing(1),
+   },
+ },
+}));
+
+const web3 = new Web3(Web3.givenProvider);
+
+const contractAddress = "0x86f707c934ac61be58ae88bb1834dae8eee8bcf9";
+const storageContract = new web3.eth.Contract(simpleStorage, contractAddress);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const classes = useStyles();
+ const [number, setUint] = useState(0);
+ const [getNumber, setGet] = useState("0");
+
+ const numberSet = async (t) => {
+   t.preventDefault();
+   const accounts = await window.ethereum.enable();
+   const account = accounts[0];
+   const gas = await storageContract.methods.set(number).estimateGas();
+   const post = await storageContract.methods.set(number).send({
+     from: account,
+     gas,
+   });
+ };
+
+ const numberGet = async (t) => {
+   t.preventDefault();
+   const post = await storageContract.methods.get().call();
+   setGet(post);
+ };
+
+ return (
+   <div className={classes.root}>
+     <Nav />
+     <div className="main">
+       <div className="card">
+         <TextField
+           id="outlined-basic"
+           label="Set your uint256:"
+           onChange={(t) => setUint(t.target.value)}
+           variant="outlined"
+         />
+         <form className="form" onSubmit={numberSet}>
+           <Button
+             variant="contained"
+             color="primary"
+             type="submit"
+             value="Confirm"
+           >
+             Confirm
+           </Button>
+           <br />
+           <Button
+             variant="contained"
+             color="secondary"
+             onClick={numberGet}
+             type="button"
+           >
+             Get your uint256
+           </Button>
+
+           {getNumber}
+         </form>
+       </div>
+     </div>
+   </div>
+ );
 }
 
 export default App;
